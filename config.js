383638feefdbd0bd0ff3edf03d34c4b27c47d562
welbstercczdev <welbsterccz@gmail.com>
2025-06-23 22:25:12 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,1 @@
-// Arquivo de Configuração - config.js
 
-// Configuração do Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyCFdMUwG6F27c8VlHLpQy3S8cM_frvQmgU",
-    authDomain: "pwa-pedidos.firebaseapp.com",
-    projectId: "pwa-pedidos",
-    storageBucket: "pwa-pedidos.appspot.com",
-    messagingSenderId: "807830113116",
-    appId: "1:807830113116:web:ce4cc262132cdbd7634f99"
-};
-
-// URLs das APIs
-const API_URLS = {
-    agentes: 'https://script.google.com/macros/s/AKfycbz2ARfFFzrRAgeq_gGfZSZ4lIOkPXOd5LOHGQpFRoSdgq65uGE4T3MTzZ2aetl_cYZUuA/exec',
-    viaCep: 'https://viacep.com.br/ws/',
-    registrarPedido: 'https://script.google.com/macros/s/AKfycbz88PwE5l8g1kJnQ69bGGMCquB3mn4C8KfauqAS0tXEoiBmKfbsBqlw-BOXQGwdszxjrQ/exec'
-};
