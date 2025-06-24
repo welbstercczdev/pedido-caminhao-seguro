3d48fc79e54c72adb47f6425e59ee30c004a6313
welbstercczdev <welbsterccz@gmail.com>
2025-06-23 22:26:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
 // sw.js - Versão com correção para APIs externas
 // INCREMENTE A VERSÃO DO CACHE PARA FORÇAR A ATUALIZAÇÃO
-// sw.js - Versão 6, com config.js
-const CACHE_NAME = 'pedido-caminhao-cache-v6'; 
+const CACHE_NAME = 'pedido-caminhao-cache-v5'; 
 
 const urlsToCache = [
   '/',
   './',
   'index.html',
   'manifest.json',
-  'favicon.ico',
-  'config.js', // <-- ARQUIVO ADICIONADO AO CACHE
+  'favicon.ico', // Adicionando o favicon ao cache
   'icons/icon-192x192.png',
   'icons/icon-512x512.png',
   'icons/icon-maskable-512x512.png'
 ];
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
